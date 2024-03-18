a17f191c6e5e68d9be20fe66af1986322564d23a
Pericles Christian <pericleschrstn@gmail.com>
2024-03-18 17:17:18 -0300

    feat: add color mode dark and light

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,46 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Favorites from "./Components/Favorites";
 import { PokemonProvider } from "./PokemonContext";
+import { ThemeProvider, createTheme } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+
+export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 const App = () => {
+  const [mode, setMode] = React.useState("light");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
   return (
     <>
       <BrowserRouter>
-        <PokemonProvider>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="favoritos" element={<Favorites />} />
-          </Routes>
-        </PokemonProvider>
+        <ColorModeContext.Provider value={colorMode}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <PokemonProvider>
+              <Header />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="favoritos" element={<Favorites />} />
+              </Routes>
+            </PokemonProvider>
+          </ThemeProvider>
+        </ColorModeContext.Provider>
       </BrowserRouter>
     </>
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,9 +3,25 @@ import Logo from "../assets/logotipo.svg?react";
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
-import { Container } from "@mui/material";
+import { Box, Container, IconButton } from "@mui/material";
+import { useTheme } from "@emotion/react";
+import { ColorModeContext } from "../App";
+import { Brightness4, Brightness7 } from "@mui/icons-material";
 
 const Header = () => {
+  function MyApp() {
+    const theme = useTheme();
+    const colorMode = React.useContext(ColorModeContext);
+    return (
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        {theme.palette.mode} tema
+        <IconButton onClick={colorMode.toggleColorMode} color="inherit">
+          {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
+      </Box>
+    );
+  }
+
   return (
     <header className={styles.header}>
       <Container>
@@ -16,11 +32,12 @@ const Header = () => {
             </Link>
           </Grid2>
           <Grid2 display="flex" alignItems="center">
-            <nav>
+            <Box component="nav" sx={{ display: "flex", alignItems: "center", gap: 2 }}>
               <NavLink to="favoritos" aria-label="Pokedex - Favoritos" className={styles.navLink}>
                 Favoritos
               </NavLink>
-            </nav>
+              <MyApp />
+            </Box>
           </Grid2>
         </Grid2>
       </Container>
